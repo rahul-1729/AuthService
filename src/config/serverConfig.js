0debc883d214326387077e5662862cee5c5204b9
rahul-1729 <rk9763981@gmail.com>
2024-07-03 07:49:43 +0530

    Added jwt create and verification

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,9 +1,11 @@
 const dotenv = require('dotenv');
-const bcrypt = require('bcrypt');
 dotenv.config();
+const bcrypt = require('bcrypt');
+
  
 module.exports={
     
-    PORT:process.env.PORT||3001,
-    SALT:bcrypt.genSaltSync(10)
-}
+    PORT: process.env.PORT,
+    SALT: bcrypt.genSaltSync(10),
+    JWT_KEY: process.env.JWT_KEY
+};
--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ const bodyParser = require('body-parser');
 
 const{PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
-const{User} = require('./models/index');
-const bcrypt = require('bcrypt');
-const UserRepository = require('./repository/user-repository');
+ 
 
+const UserService = require('./services/user-service');
 const prepareAndStartServer=()=>{
     
     app.use(bodyParser.json());
@@ -16,17 +15,10 @@ const prepareAndStartServer=()=>{
 
     app.listen(PORT,async()=>{
         console.log(`Server started on Port: ${PORT}`);
-
-        
-        // const repo = new UserRepository();
-        // const response = await repo.getById(3);
-        // console.log(response);
-
-
-        // const incomingpassword ='123456';
-        // const user= await User.findByPk(3);
-        // const response = bcrypt.compareSync(incomingpassword,user.password);
-        // console.log(response)
+       
+        const service = new UserService();
+        const newToken = service.createToken({email:'rk9763981@gmail.com',id:2});
+        console.log("new token is ",newToken)
 
     })
 }
